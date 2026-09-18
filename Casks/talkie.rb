07cb3899c37a90cad4bd563363e0703119d3618a
Arach <arach@tchoupani.com>
2026-09-18 14:24:46 -0400

    talkie 2.6.0

--- Casks/talkie.rb
+++ Casks/talkie.rb
@@ -1,6 +1,6 @@
 cask "talkie" do
-  version "2.5.44"
-  sha256 "8c7c92755a0a074a40ca8a9a629485b03a15552cd1da6a4a28603132f98610f6"
+  version "2.6.0"
+  sha256 "15cbebf4baf1743e4cc7dbff35173daa88d0c1d6a85f4cc5a34ae7c92c63e966"
 
   url "https://github.com/arach/usetalkie.com/releases/download/v#{version}/Talkie.dmg"
   name "Talkie"
